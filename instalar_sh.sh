16de90803c44a45fcd91a113a715a85d62ac0d95
Alesander21789 <39167112+Alesander21789@users.noreply.github.com>
2023-02-10 08:32:37 +0100

    AAAAAAAA

---
+++ instalar_sh.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "Starting SSH installation and configuration..."
+
+# Check if root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
+fi
+
+# Prompt for SSH port
+echo -n "Enter the desired SSH port (default 22): "
+read ssh_port
+
+# Set default SSH port if not provided
+if [ -z "$ssh_port" ]; then
+  ssh_port=22
+fi
+
+# Prompt for root login permission
+echo -n "Allow root login? (y/n, default n): "
+read root_login
+
+# Set default root login permission if not provided
+if [ -z "$root_login" ]; then
+  root_login="n"
+fi
+
+# Install SSH
+apt-get update
+apt-get install openssh-server -y
+
+# Backup original SSH config
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+
+# Configure SSH
+sed -i "s/#Port 22/Port $ssh_port/g" /etc/ssh/sshd_config
+if [ "$root_login" = "y" ]; then
+  sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+else
+  sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config
+fi
+
+# Enable key-based authentication
+sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
+sed -i "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
+
+# Restart SSH
+systemctl restart ssh
+
+echo "SSH installation and configuration complete!"
