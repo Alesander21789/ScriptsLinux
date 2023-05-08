468b4a50d92acf1046dac2ec2e384b8ba02bea53
DESKTOP-T1ATEEO\Alesander <sanderfene21789@gmail.com>
2023-05-08 21:22:12 +0200

    no message

--- ScriptsForense/montarEFW.sh
+++ ScriptsForense/montarEFW.sh
@@ -26,3 +26,36 @@ cleanup() {
 }
 
 trap cleanup EXIT
+
+#!/bin/bash
+
+# Verificar si el archivo EFW existe
+if [ ! -f "$1" ]; then  
+  echo "El archivo $1 no existe."
+  exit 1
+fi
+
+# Verificar si se ha especificado una ruta de montaje
+if [ -z "$2" ]; then
+  echo "Debe especificar una ruta de montaje."
+  exit 1
+fi
+
+# Crear un dispositivo loopback
+loop_device=$(sudo losetup -f)
+sudo losetup $loop_device $1
+
+# Mostrar el loop creado
+echo "El archivo $1 ha sido montado en el loop $loop_device"
+
+# Montar el dispositivo loopback sin ejecutar archivos
+sudo mount -o ro,noload $loop_device "$2"
+
+# Mostrar el contenido del archivo EFW
+ls -la "$2"
+
+# Desmontar el dispositivo loopback
+sudo umount "$2"
+sudo losetup -d $loop_device
+
+
